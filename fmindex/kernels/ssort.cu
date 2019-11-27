2269cb8dd46729031397f70364e382823a235aa8
Hsuan-Ting Lu <hsuan.ting.lu.ee05@g2.nctu.edu.tw>
2019-11-27 13:22:33 +0800

    Project requires two applications to be merged together
    Bootstrapped subproject: fmindex

--- fmindex/kernels/ssort.cu
+++ fmindex/kernels/ssort.cu
@@ -1,11 +1,7 @@
 /*
- * Host side master program
+ * String Sorting CUDA kernel
  * Copyright (C) 2019  Hsuan-Ting Lu
  *
  * GNU General Public License v3.0+
  * (see LICENSE or https://www.gnu.org/licenses/)
  */
-
-int main() {
-
-}
