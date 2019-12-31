48940c2a8d5a906f3ffe8aff47724c756e3d28d8
Hsuan-Ting Lu <hsuan.ting.lu.ee05@g2.nctu.edu.tw>
2019-12-31 18:43:23 +0800

    Adapt to new project specifications
    Change of DNA read length from 64 to 63
    
    Single thread radix sort with parallel-write array ready

--- fmindex/main.cpp
+++ fmindex/main.cpp
@@ -27,14 +27,14 @@
 // clang-format on
 
 // TODO: pin memory
-void read_input(std::ifstream* ifs, entry* array, char (*TA_array)[65],
+void read_input(std::ifstream* ifs, entry* array, char (*TA_array)[64],
                 const int INPUTSIZE) {
-    char buffer[65];
-    buffer[64] = '$';
+    char buffer[64];
+    buffer[63] = '$';
     for (int str_idx = 0; str_idx != INPUTSIZE; ++str_idx) {
-        ifs->read(buffer, 64);
+        ifs->read(buffer, 63);
         ifs->ignore();
-        std::memcpy(TA_array[str_idx], buffer, 65);
+        std::memcpy(TA_array[str_idx], buffer, 64);
         array[str_idx] = entry(buffer);
     }
 }
@@ -49,7 +49,7 @@ int main(int argc, char** argv) {
     const int INPUTSIZE = std::count(std::istreambuf_iterator<char>(ifs),
                                      std::istreambuf_iterator<char>(), '\n');
     ifs.seekg(0);  // rewind
-    const int EXPANDEDSIZE = 65 * INPUTSIZE;
+    const int EXPANDEDSIZE = 64 * INPUTSIZE;
     std::cerr << "expected output size :: str_array: " << INPUTSIZE
               << ", rotate_expand: " << EXPANDEDSIZE << "\n";
 
@@ -58,10 +58,10 @@ int main(int argc, char** argv) {
     entry_repr::origin = str_array;
     entry_repr* repr_array = new entry_repr[EXPANDEDSIZE];
     // allocate TA's array
-    auto TA_str_array = new char[INPUTSIZE][65];
+    auto TA_str_array = new char[INPUTSIZE][64];
     auto TA_suffixes = new char**[INPUTSIZE];  // expanded string array
     auto TA_L = new char[EXPANDEDSIZE];
-    auto TA_F_counts = new int[4]{0, 0, 0, 0};
+    int TA_F_counts[4] = {0, 0, 0, 0};
     int** TA_SA_Final = nullptr;
     int** TA_L_counts = nullptr;
     // TA's structures for correctness check
@@ -70,7 +70,7 @@ int main(int argc, char** argv) {
     char* student_L = new char[EXPANDEDSIZE];
     int student_F_counts[4] = {0, 0, 0, 0};
     // Init TA's structures
-    for(int i=0; i!=EXPANDEDSIZE; ++i) {
+    for (int i = 0; i != EXPANDEDSIZE; ++i) {
         auto SA = student_SA_Final[i];
         SA[0] = 0;
         SA[1] = 0;
@@ -99,9 +99,9 @@ int main(int argc, char** argv) {
         std::cerr << "Measure TA time\n";
         // FIXME: TA starts FM-index generation
         for (int i = 0; i != INPUTSIZE; ++i) {
-            TA_suffixes[i] = generateSuffixes(TA_str_array[i], 65);
+            TA_suffixes[i] = generateSuffixes(TA_str_array[i], 64);
         }
-        TA_L_counts = makeFMIndex(TA_suffixes, INPUTSIZE, 65, TA_F_counts, TA_L,
+        TA_L_counts = makeFMIndex(TA_suffixes, INPUTSIZE, 64, TA_F_counts, TA_L,
                                   TA_SA_Final);
     }
     auto TA_timer_end = std::chrono::high_resolution_clock::now();
@@ -124,15 +124,15 @@ int main(int argc, char** argv) {
     sort::expand_rotation(INPUTSIZE, repr_array);
     std::cout << "post expansion" << std::endl;
     /*for (int i = 0; i != EXPANDEDSIZE; ++i) {
-        if (!(i % 65)) {
-            std::cout << "< " << i / 65 << " >\n";
+        if (!(i % 64)) {
+            std::cout << "< " << i / 64 << " >\n";
         }
         std::cout << repr_array[i] << " " << (unsigned
     int)(repr_array[i].str_shift) << std::endl;
     }*/
 
     // Scan for distribution
-    unsigned int partition_freq[sort::PARTITION_SIZE] = {0};
+    unsigned int partition_freq[sort::PARTITION_SIZE] = {0U};
 
     // Partition
     std::cout << "do partition" << std::endl;
@@ -147,48 +147,49 @@ int main(int argc, char** argv) {
     std::cerr << "check sorting\n";
     std::future<void> sort_work[sort::PARTITION_SIZE];
     for (unsigned int part = 0; part != sort::PARTITION_SIZE; ++part) {
-        entry_repr* subarray_head =
-            repr_array +
+        unsigned int section_shift =
             std::accumulate(partition_freq, partition_freq + part, 0);
+        entry_repr* subarray_head = repr_array + section_shift;
         unsigned int subarray_size = partition_freq[part];
         std::cout << "Start sorting sub-section " << part
+                  << ", shift = " << section_shift
                   << ", size = " << subarray_size << std::endl;
-
         sort_work[part] = std::async(
             std::launch::async, [subarray_head, subarray_size]() -> void {
                 sort::radix_sort(subarray_head, subarray_size);
             });
     }
+    // sort::radix_sort(repr_array, EXPANDEDSIZE);
     for (unsigned int part = 0; part != sort::PARTITION_SIZE; ++part) {
         sort_work[part].wait();
         std::cout << "Finish sorting sub-section " << part << std::endl;
     }
 
     std::cout << "post sorting" << std::endl;
-    /*for (int i = 0; i != EXPANDEDSIZE; ++i) {
+    for (int i = 0; i != EXPANDEDSIZE; ++i) {
         std::cout << repr_array[i] << std::endl;
-    }*/
+    }
 
     // FIXME: Fulfill TA's specifications
-    for(int i=0; i!=4; ++i) {
+    for (int i = 0; i != 4; ++i) {
         student_F_counts[i] = partition_freq[i];
     }
-    for(int i=0; i!=EXPANDEDSIZE; ++i) {
+    for (int i = 0; i != EXPANDEDSIZE; ++i) {
         entry_repr repr = repr_array[i];
         uint8_t* string = (repr.origin[repr.str_idx]).data;
-        uint8_t L = string[(repr.str_shift+64)%65];
+        uint8_t L = string[(repr.str_shift + 63) % 64];
         student_L[i] = utils::reverse_char(L);
         student_SA_Final[i][0] = repr.str_shift;
         student_SA_Final[i][1] = repr.str_idx;
 
         if (i != 0) {
-            student_L_counts[i][0] = student_L_counts[i-1][0];
-            student_L_counts[i][1] = student_L_counts[i-1][1];
-            student_L_counts[i][2] = student_L_counts[i-1][2];
-            student_L_counts[i][3] = student_L_counts[i-1][3];
+            student_L_counts[i][0] = student_L_counts[i - 1][0];
+            student_L_counts[i][1] = student_L_counts[i - 1][1];
+            student_L_counts[i][2] = student_L_counts[i - 1][2];
+            student_L_counts[i][3] = student_L_counts[i - 1][3];
         }
         if (static_cast<unsigned int>(L) != 0) {
-            student_L_counts[i][static_cast<unsigned int>(L)-1] += 1;
+            student_L_counts[i][static_cast<unsigned int>(L) - 1] += 1;
         }
     }
 
@@ -212,12 +213,12 @@ int main(int argc, char** argv) {
         std::cout << "TA code spent: " << TA_time_spent << " s" << std::endl;
 
         double speedup = 0.0;
-        if (checker(INPUTSIZE, 65, student_L, student_SA_Final,
+        if (checker(INPUTSIZE, 64, student_L, student_SA_Final,
                     student_L_counts, student_F_counts, TA_L, TA_SA_Final,
                     TA_L_counts, TA_F_counts) == 1) {
             speedup = TA_time_spent / student_time_spent;
         }
-        //speedup = TA_time_spent / student_time_spent; // DEBUG:
+        // speedup = TA_time_spent / student_time_spent; // DEBUG:
         std::cout << "Speedup=" << speedup << std::endl;
     }
 
